21540cd5dbf045a81b18117c1af42207d4acedd8
NadeesH <spnadeeshnimsara@gmail.com>
2024-02-10 17:24:57 +0530

    Update report route

--- backend/routes/reports.js
+++ backend/routes/reports.js
@@ -11,6 +11,92 @@ const router = express.Router();
 // @desc    get report
 // @access  Private
 router.post('/', auth, async (req, res) => {
+  const ordersByDate = await Order.aggregate([
+    {
+      $match: {
+        $expr: {
+          $eq: [{ $month: '$date' }, { $month: new Date() }],
+        },
+      },
+    },
+    {
+      $unwind: '$products',
+    },
+    {
+      $group: {
+        _id: { $dateToString: { format: '%Y-%m-%d', date: '$date' } },
+        Sales: {
+          $sum: { $multiply: ['$products.price', '$products.quantity'] },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        date: '$_id',
+        Sales: 1,
+      },
+    },
+  ]).sort({ date: 1 });
+
+  const customersByDate = await User.aggregate([
+    {
+      $match: {
+        $expr: {
+          $eq: [{ $month: '$date' }, { $month: new Date() }],
+        },
+      },
+    },
+    {
+      $group: {
+        _id: { $dateToString: { format: '%Y-%m-%d', date: '$date' } },
+        Customers: {
+          $count: {},
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        date: '$_id',
+        Customers: 1,
+      },
+    },
+  ]).sort({ date: 1 });
+
+  let date = new Date();
+  date.setDate(1);
+
+  let performance = [];
+
+  ordersByDate.forEach((order) => {
+    while (date.getDate() <= new Date().getDate()) {
+      if (order.date === date.toISOString().split('T')[0]) {
+        performance.push({
+          Customers: 0,
+          Sales: order.Sales,
+          date: order.date,
+        });
+        date.setDate(date.getDate() + 1);
+        break;
+      } else {
+        performance.push({
+          Customers: 0,
+          Sales: 0,
+          date: date.toISOString().split('T')[0],
+        });
+        date.setDate(date.getDate() + 1);
+      }
+    }
+  });
+
+  res.status(200).json(performance);
+});
+
+// @route   POST api/report
+// @desc    get report
+// @access  Private
+router.post('/', auth, async (req, res) => {
   const customersByDate = await User.aggregate([
     {
       $group: {
